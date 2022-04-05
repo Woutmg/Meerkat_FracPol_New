32373801867f4f3776d677e369d1892ce07306f4
Wout Goesaert <goesaert@zuidplas.strw.leidenuniv.nl>
2022-04-05 23:05:54 +0200

    Tried to get rmsynth working

--- polarization_analysis.py
+++ polarization_analysis.py
@@ -10,7 +10,6 @@ from astropy import wcs
 from matplotlib.patches import Ellipse
 import tqdm
 
-
 """
 Analyze polarization data. See also rmsynth_G280.19+47.81.py
 
@@ -27,7 +26,7 @@ using casa
 if __name__ == '__main__':
     plt.ioff() # To not show plots immediately
     c = 299792458 # m / s
-    nchan = 90#48
+    nchan = 12
     print ("Assuming %i channels"%nchan)
 
 
@@ -105,7 +104,7 @@ def max_scale(nu_max):
     lambda_min = c/nu_max
     return np.pi/lambda_min**2
 
-def plot_rms_vs_spw(directory, sourcename, title, savefig):
+def plot_rms_vs_spw(title, savefig):
     """
     Determine RMS noise as the RMS of the entire (large) Q or U image.
 
@@ -123,31 +122,29 @@ def plot_rms_vs_spw(directory, sourcename, title, savefig):
         if channum%6 == 0 and channum != 0:
             # Average all images in the same spw
             spw = int(channum/6-1)
-            immath(outfile=('./%s/%s/'%(directory,sourcename)
+            immath(outfile=('/net/vdesk/data2/GoesaertW/Meerkat_Data/Abell_85/'
                             +'averaged_images/averaged_Q_spw%i'%spw)
                     ,mode='evalexpr'
                     ,imagename=imagesQ
                     ,expr=exp_avg)
-            immath(outfile=('./%s/%s/'%(directory,sourcename)
+            immath(outfile=('/net/vdesk/data2/GoesaertW/Meerkat_Data/Abell_85/'
                             +'averaged_images/averaged_U_spw%i'%spw)
                     ,mode='evalexpr'
                     ,imagename=imagesU
                     ,expr=exp_avg)
 
-            all_noiseQ[spw] = imstat(('./%s/%s/'%(directory,sourcename)
-                                      +'averaged_images/averaged_Q_spw%i'%spw))['rms']
-            all_noiseU[spw] = imstat(('./%s/%s/'%(directory,sourcename)
-                                      +'averaged_images/averaged_U_spw%i'%spw))['rms']
+            all_noiseQ[spw] = imstat('/net/vdesk/data2/GoesaertW/Meerkat_Data/Abell_85/'
+                                      +'averaged_images/averaged_Q_spw%i'%spw)['rms']
+            all_noiseU[spw] = imstat('/net/vdesk/data2/GoesaertW/Meerkat_Data/Abell_85/'
+                                      +'averaged_images/averaged_U_spw%i'%spw)['rms']
 
             imagesQ = []
             imagesU = []
 
-        imnameQ = ('./%s/%s/'%(directory,sourcename)
-                  +'stokes_q/{}'.format(sourcename)
-                  +'_{0:04}-Q-image.pbcor.smoothed.fits'.format(channum))
-        imnameU = ('./%s/%s/'%(directory,sourcename)
-                  +'stokes_u/{}'.format(sourcename)
-                  +'_{0:04}-U-image.pbcor.smoothed.fits'.format(channum))
+        imnameQ = ('/net/vdesk/data2/GoesaertW/Meerkat_Data/Abell_85/Q_slices/'
+                  +'Abell_85_Q_plane_freq'+str(channum+1)+'.fits')
+        imnameU = ('/net/vdesk/data2/GoesaertW/Meerkat_Data/Abell_85/U_slices/'
+                  +'Abell_85_U_plane_freq'+str(channum+1)+'.fits')
         imagesQ.append(imnameQ)
         imagesU.append(imnameU)
 
@@ -155,20 +152,20 @@ def plot_rms_vs_spw(directory, sourcename, title, savefig):
     # Don't forget the last one.
     channum += 1
     spw = int(channum/6-1)
-    immath(outfile=('./%s/%s/'%(directory,sourcename)
+    immath(outfile=('/net/vdesk/data2/GoesaertW/Meerkat_Data/Abell_85/'
                     +'averaged_images/averaged_Q_spw%i'%spw)
             ,mode='evalexpr'
             ,imagename=imagesQ
             ,expr=exp_avg)
-    immath(outfile=('./%s/%s/'%(directory,sourcename)
+    immath(outfile=('/net/vdesk/data2/GoesaertW/Meerkat_Data/Abell_85/'
                     +'averaged_images/averaged_U_spw%i'%spw)
             ,mode='evalexpr'
             ,imagename=imagesU
             ,expr=exp_avg)
-    all_noiseQ[spw] = imstat(('./%s/%s/'%(directory,sourcename)
-                              +'averaged_images/averaged_Q_spw%i'%spw))['rms']
-    all_noiseU[spw] = imstat(('./%s/%s/'%(directory,sourcename)
-                              +'averaged_images/averaged_U_spw%i'%spw))['rms']   
+    all_noiseQ[spw] = imstat('/net/vdesk/data2/GoesaertW/Meerkat_Data/Abell_85/'
+                              +'averaged_images/averaged_Q_spw%i'%spw)['rms']
+    all_noiseU[spw] = imstat('/net/vdesk/data2/GoesaertW/Meerkat_Data/Abell_85/'
+                              +'averaged_images/averaged_U_spw%i'%spw)['rms']
     all_spw = np.arange(16)
 
     plt.title(title)
@@ -181,28 +178,7 @@ def plot_rms_vs_spw(directory, sourcename, title, savefig):
     plt.show()
     plt.close()
 
-def manually_add_failedchannels(sourcename, failedchannels):
-    """
-    While writing the paper, I noticed that some channels were affected by RFI
-    but those channels were not flagged by the code below. So we flag it manually
-    """
-    manually = np.array([])
-    if sourcename == 'G114.33+64.87':
-        manually = np.array([20, 21, 22, 23, 25, 27, 28])
-    if sourcename == 'G062.42-46.41':
-        failedchannels = np.array([0,1,2,3,6,8,14,17,22])
-    if sourcename == 'G186.39+37.25':
-        failedchannels = np.array([19,21,22,48,49,50,51,52,53])
-    if sourcename == 'G077.90-26.64':
-        failedchannels = np.array([6,7,9,10,11,18,22,48,49,50,51,52])
-    if sourcename == 'G072.63+41.46':
-        failedchannels = np.array([20,21,22,23,48,50,51,52])
-
-    failedchannels = np.append(failedchannels, manually)
-    failedchannels = np.unique(failedchannels) # Just in case I manually add something that was already in ther
-    return failedchannels
-
-def plot_rms_vs_channel(directory, sourcename, title, savefig):
+def plot_rms_vs_channel(title, savefig):
     """
     Determine RMS noise as the central RMS I, Q or U image.
 
@@ -213,55 +189,26 @@ def plot_rms_vs_channel(directory, sourcename, title, savefig):
     all_noiseQ = np.empty(nchan) #(90)
     all_noiseU = np.empty(nchan) #(90)
     all_noiseI = np.empty(nchan) #(90)
-
-    # Ignore the failed channels
-    failedchannels = np.load('./%s/%s/failedchannels.npy'%(directory,sourcename))
     
     # Use hinges-fences with fence=1.0 so we cut the highest and lowest values
     # from the img
-    box = '1100,1100,1500,1500' # Calculate central rms
+    box = '1608,1608,1608,1608' # Calculate central rms, changed by WoutG to mkt images
     # algorithm = 'classic'
     algorithm = 'hinges-fences'
 
-    for channum in tqdm.tqdm(range(0,nchan),desc="Calculating rms per channel for target %s"%sourcename):
-        if channum in failedchannels:
-            all_noiseQ[channum] = np.nan
-            all_noiseU[channum] = np.nan
-            all_noiseI[channum] = np.nan
-        else:
-            rmsQ = imstat(('./{}/{}/{}/{}'.format(
-                                directory,sourcename,'stokes_q',sourcename)
-                                +'_%04d-Q-image.pbcor.smoothed.fits'%channum)
+    for channum in tqdm.tqdm(range(0,nchan),desc='Calculating rms per channel'):
+        print(channum)
+        rmsQ = imstat('/net/vdesk/data2/GoesaertW/Meerkat_Data/Abell_85/Q_slices/Abell_85_Q_plane_freq'+str(channum+1)+'.fits'
                                 ,algorithm=algorithm,fence=1.0,box=box)['rms'][0]
-            rmsU = imstat(('./{}/{}/{}/{}'.format(
-                        directory,sourcename,'stokes_u',sourcename)
-                                +'_%04d-U-image.pbcor.smoothed.fits'%channum)
+        rmsU = imstat('/net/vdesk/data2/GoesaertW/Meerkat_Data/Abell_85/U_slices/Abell_85_U_plane_freq'+str(channum+1)+'.fits'
                                 ,algorithm=algorithm,fence=1.0,box=box)['rms'][0]
-            rmsI = imstat(('./{}/{}/{}/{}'.format(
-                        directory,sourcename,'stokes_i',sourcename)
-                                +'_%04d-image.pbcor.smoothed.fits'%channum)
+        rmsI = imstat('/net/vdesk/data2/GoesaertW/Meerkat_Data/Abell_85/I_slices/Abell_85_I_plane_freq'+str(channum+1)+'.fits'
                                 ,algorithm=algorithm,fence=1.0,box=box)['rms'][0]
 
-            if rmsQ > 2e-3 or rmsU > 2e-3: 
-                # If RMS above 2 mJy then say it's a failed channel
-                # Usually RMS is around 200 microJansky (in single channel)
-                failedchannels = np.append(failedchannels,channum)
 
-            else:
-                all_noiseQ[channum] = rmsQ
-                all_noiseU[channum] = rmsU
-                all_noiseI[channum] = rmsI
-        
-    # Perhaps manually add channels affected by RFI as failedchannels
-    failedchannels = manually_add_failedchannels(sourcename, failedchannels)
-    # Save as RMS for failed channels as NaN so we're sure it doesnt get used
-    all_noiseQ[np.array(failedchannels,dtype='int')] = np.nan
-    all_noiseU[np.array(failedchannels,dtype='int')] = np.nan
-    all_noiseI[np.array(failedchannels,dtype='int')] = np.nan
-
-    # Save the new version of failed channels in case anything changed
-    failedchannels = np.sort(failedchannels)
-    np.save('./%s/%s/failedchannels.npy'%(directory,sourcename),failedchannels)
+        all_noiseQ[channum] = rmsQ
+        all_noiseU[channum] = rmsU
+        all_noiseI[channum] = rmsI
 
     # plt.scatter(all_chan,all_noiseQ*1e6,label='{}'.format(sourcename),alpha=0.5)
     plt.scatter(all_chan,all_noiseQ*1e6,alpha=0.5,label='Stokes Q',c='#1f77b4')
@@ -269,9 +216,10 @@ def plot_rms_vs_channel(directory, sourcename, title, savefig):
     plt.scatter(all_chan,all_noiseI*1e6,alpha=0.5,marker="s",label='Stokes I',c='#ff7f0e')
     plt.axhline(np.nanmedian(all_noiseI)*1e6,ls='dashed',c='k',label='Stokes I median noise')
 
-    np.save('./%s/%s/all_noiseQ.npy'%(directory,sourcename),all_noiseQ)
-    np.save('./%s/%s/all_noiseU.npy'%(directory,sourcename),all_noiseU)
-    np.save('./%s/%s/all_noiseI.npy'%(directory,sourcename),all_noiseI)
+    np.save('/net/vdesk/data2/GoesaertW/Meerkat_Data/Abell_85/all_noiseQ.npy',all_noiseQ)
+    np.save('/net/vdesk/data2/GoesaertW/Meerkat_Data/Abell_85/all_noiseU.npy',all_noiseU)
+    np.save('/net/vdesk/data2/GoesaertW/Meerkat_Data/Abell_85/all_noiseI.npy',all_noiseI)
+
 
     plt.xlabel("Channel index")
     plt.title(title)
@@ -281,44 +229,13 @@ def plot_rms_vs_channel(directory, sourcename, title, savefig):
     plt.show()
     plt.close()
 
-def remove_failed_channels(directory, sourcename):
-    """
-    Because the function plot_rms_vs_channel() edits the failedchannels
-    we have to remove the new failed channels from the stokes_{i,q,u} directories.
-    Otherwise the RMsynth module will still use them
-    """
-
-
-    pre_imname = str(directory) +'/'+ sourcename + '/' 
-    output_dir = pre_imname + 'stokes_'
-
-    print ("Removing all pb corrected smoothed images that are failed from %s"%(output_dir+"(q,u,i)"))
-
-    stokes = ['i','q','u']
-    imagestokes = ['','Q-','U-']
-    MFSimagestokes = ['I-', 'Q-', 'U-'] # naming is slightly different...
-
-    failedchannels = np.load('./%s/%s/failedchannels.npy'%(directory,sourcename))
-
-    for i, stok in enumerate(stokes):
-        for channum in range(0,nchan):
-            if channum in failedchannels:
-                    imname = output_dir+stok + '/'
-                    imname += sourcename+'_'+'{0:04}-'.format(channum) + imagestokes[i] +'image.pbcor.smoothed.fits'  
-                    if os.path.exists(imname):
-                        removecommand = 'rm ' + imname
-                        print (removecommand)
-                        os.system(removecommand)
-
-def create_inverse_variance_weights(directory, sourcename):
+def create_inverse_variance_weights():
     """
     Given the rms noise levels that were just calculated. Translate these
     to weights.
     """
-
-
-    all_noiseQ = np.load('./%s/%s/all_noiseQ.npy'%(directory,sourcename))
-    all_noiseU = np.load('./%s/%s/all_noiseU.npy'%(directory,sourcename))
+    all_noiseQ = np.load('/net/vdesk/data2/GoesaertW/Meerkat_Data/Abell_85/all_noiseQ.npy')
+    all_noiseU = np.load('/net/vdesk/data2/GoesaertW/Meerkat_Data/Abell_85/all_noiseU.npy')
     # Remove failed channels if any
     nanmask = np.invert(np.isnan(all_noiseQ))
     
@@ -333,7 +250,7 @@ def create_inverse_variance_weights(directory, sourcename):
     weights /= np.sum(weights)
 
     # save the weights to a file
-    weightfile = open('./%s/%s/inv_var_weights.txt'%(directory,sourcename),'w')
+    weightfile = open('/net/vdesk/data2/GoesaertW/Meerkat_Data/Abell_85/inv_var_weights.txt','w')
     for i, weight in enumerate(weights):
         weightfile.write(str(weight))
         if i != len(weights)-1:
@@ -351,11 +268,11 @@ def plotRMTF(rmtfile, savefig=None, show=False):
     if show: plt.show()
     plt.close()
 
-def fix_rmsynth_polint_header(directory, sourcename):
+def fix_rmsynth_polint_header():
     """
     For PYBDSF to accept the file, need at least 3 axis where the third is freq
     """
-    rmpolint = './%s/%s/rmsynth/rmsynth_polint.fits'%(directory,sourcename)
+    rmpolint = '/net/vdesk/data2/GoesaertW/Meerkat_Data/Abell_85/rmsynth/rmsynth_polint.fits'
 
     with fits.open(rmpolint,mode='update') as hdul:
         head = hdul[0].header
@@ -370,12 +287,12 @@ def fix_rmsynth_polint_header(directory, sourcename):
         hdul[0].header = head
         hdul.flush()
 
-def create_parameterfile(directory, sourcename):
+def create_parameterfile():
     
     # Open any image to get the image size
-    img = glob.glob('./{}/{}/{}/{}_00*-Q-image.pbcor.smoothed.fits'.format(directory,sourcename,'stokes_q',sourcename))[0]
+    img = glob.glob('/net/vdesk/data2/GoesaertW/Meerkat_Data/Abell_85/I_slices/Abell_85_I_plane_freq1.fits')[0]
     with fits.open(img) as hdul:
-        size = hdul[0].data.shape[2]
+        size = hdul[0].data.shape[1]
 
     with open(rmsynthpath+'rmsynth.par','w') as file:
         # This is a comment
@@ -386,14 +303,14 @@ def create_parameterfile(directory, sourcename):
         file.write('\n')
         file.write(r'% a value of -1 means to use the bound of the image')
         file.write('\n')
-        if size == 2736:
-            file.write(r'dec_min 605')
+        if size == 3617:
+            file.write(r'dec_min 0')
             file.write('\n')
-            file.write(r'dec_max 2125')
+            file.write(r'dec_max 3617')
             file.write('\n')
-            file.write(r'ra_min 610')
+            file.write(r'ra_min 0')
             file.write('\n')
-            file.write(r'ra_max 2120')
+            file.write(r'ra_max 3617')
             file.write('\n')
         else:
             raise ValueError("SIZE %s NOT IMPLEMENTED YET"%size)
@@ -401,11 +318,11 @@ def create_parameterfile(directory, sourcename):
         file.write('\n')
         file.write(r'% Define the phi axis, dphi in rad/m/m')
         file.write('\n')
-        file.write(r'phi_min -2000')
+        file.write(r'phi_min -1600')
         file.write('\n')
-        file.write(r'nphi 400')
+        file.write(r'nphi 32')
         file.write('\n')
-        file.write(r'dphi 10')
+        file.write(r'dphi 100')
         file.write('\n')
         file.write('\n')
         
@@ -441,10 +358,10 @@ def create_parameterfile(directory, sourcename):
 
         file.write(r'% directory where the input fits file can be found')
         file.write('\n')
-        file.write('input_dir ./%s/%s/'%(directory,sourcename))
+        file.write('/net/vdesk/data2/GoesaertW/Meerkat_Data/Abell_85/')
         file.write('\n')
 
-def cleanup_rmsynth(directory, sourcename):
+def cleanup_rmsynth():
     """
     Remove the Faraday dispersion function cubes. F(\phi)
         (i.e., P(\phi),Q(\phi) and U(\phi))
@@ -467,21 +384,16 @@ def cleanup_rmsynth(directory, sourcename):
 
 if __name__ == '__main__':
 
-    # Call this script with .e.g., casa -c *.py 81019 G134.73+48.89
-    try:
-        directory, sourcename = int(sys.argv[3]), str(sys.argv[4])
-    except:
-        print("ERROR: Please call this script with arguments 'directory' and 'sourcename'")
-        raise
-
+    # Call this script with .e.g., casa -c *.py
+    
     print ("Making inverse variance weights and running RM synthesis.")
 
-    figpath = './%s/%s/figures/'%(directory,sourcename)
+    figpath = r'/net/vdesk/data2/GoesaertW/Meerkat_Data/Abell_85/'
     if not os.path.exists(figpath):
         print ("Creating directory %s"%figpath)
         os.mkdir(figpath)
 
-    rmsynthpath = './%s/%s/rmsynth/'%(directory,sourcename)
+    rmsynthpath = r'/net/vdesk/data2/GoesaertW/Meerkat_Data/Abell_85/rmsynth/'
     if not os.path.exists(rmsynthpath):
         print ("Creating directory %s"%rmsynthpath)
         os.mkdir(rmsynthpath)
@@ -495,31 +407,25 @@ if __name__ == '__main__':
     # savefig = figpath+'rms_vs_spw.png'
     # plot_rms_vs_spw(directory, sourcename, title, savefig)
 
-    title = 'RMS as function of channel. %s'%sourcename
+    title = 'RMS as function of channel.'
     savefig = figpath+'rms_vs_channel.png'
-    plot_rms_vs_channel(directory, sourcename, title, savefig)
-
-    # Remove failed channels if they are new from the RMS cutoff
-    remove_failed_channels(directory, sourcename)
-
-    # Save weights as numpy array to disk
-    create_inverse_variance_weights(directory, sourcename)
+    plot_rms_vs_channel(title, savefig)
 
     # Create rmsynth.par file in ./{directory}/{sourcename}/rmsynth/rmsynth.par
-    create_parameterfile(directory,sourcename)
+    create_parameterfile()
 
     # -p for plotting the RMTF as soon as its computed
     # runrmsynth = "python /net/bovenrijn/data1/digennaro/software/pyrmsynth/rmsynthesis.py -s -p "
-    runrmsynth = "python /net/lofar4/data1/osinga/software/pyrmsynth/rmsynthesis.py -s "
+    runrmsynth = "python /net/vdesk/data2/GoesaertW/Data_Analyis_Git/rmsynthesis.py -s "
     runrmsynth += rmsynthpath+'rmsynth.par'
 
     # Actually run RM synth
     os.system(runrmsynth)
 
     # Cleanup the cubes (they are quite big). Only need the 2D maps.
-    cleanup_rmsynth(directory, sourcename)
+    cleanup_rmsynth()
 
     # Plot RMTF afterwards
     savefig = figpath+'RMTF.png'
-    plotRMTF('./%s/%s/rmsynth/rmsynth_rmsf.txt'%(directory,sourcename),savefig,show=False)
+    plotRMTF('/net/vdesk/data2/GoesaertW/Meerkat_Data/Abell_85/rmsynth/rmsynth_rmsf.txt',savefig,show=False)
     # plotRMTF('/net/voorrijn/data2/osinga/pipeline/cubes/81019/G134.73+48.89/rmsynth_inv_var/rmsynth_rmsf.txt')
